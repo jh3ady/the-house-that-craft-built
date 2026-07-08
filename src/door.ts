d153e8fa3aba0fe9ac70b19c4b71a586e8b6392b
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2026-07-08 23:53:36 +0200

    ✨ feat(house): refuse to open a locked door

--- src/door.ts
+++ src/door.ts
@@ -1,8 +1,19 @@
+export class DoorIsLockedError extends Error {
+  constructor() {
+    super('A locked door cannot be opened.')
+    this.name = 'DoorIsLockedError'
+  }
+}
+
 export class Door {
   private opened = false
   private locked = false
 
   open() {
+    if (this.locked) {
+      throw new DoorIsLockedError()
+    }
+
     this.opened = true
   }
 
