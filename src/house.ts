f64462a904005ea248a820abe117cc1ff487a322
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2026-07-08 22:30:08 +0200

    ✨ feat(house): open the house's door
    refs #1

--- src/house.ts
+++ src/house.ts
@@ -3,6 +3,10 @@ import { Door } from './door'
 export class House {
   private readonly door = new Door()
 
+  openDoor() {
+    this.door.open()
+  }
+
   isDoorOpen() {
     return this.door.isOpen()
   }
